045c48ce0320c0975791e57ccffa5a3085e174f3
estebancamargo <158587958+estebancamargo@users.noreply.github.com>
2026-05-19 11:20:01 -0500

    Update ejercicio3.cs

--- ejercicio3.cs
+++ ejercicio3.cs
@@ -1,16 +1,12 @@
 using System;
-class ejercicio3 {
-  static void Main() {
-     double myDouble = 9.78;
-     
-     int edad;
-     string nombre;
-     
-     Console.WriteLine("Ingrese su nombre: ");
-     nombre = Console.ReadLine();
-     Console.WriteLine("Ingrese su edad: ");
-     edad = Convert.ToInt16(Console.ReadLine());
-     Console.WriteLine("Su nombre es: " + nombre);
-     Console.WriteLine("Su edad es: " + edad);
-  }
+
+public class ejercicio3
+{
+    public static void Main(string[] args)
+    {
+        //int x = 100 + 50;
+        double x = 3.5 + 4.75;
+        
+        Console.WriteLine(x);
+    }
 }
