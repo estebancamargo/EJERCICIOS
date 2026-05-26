99828426be889ed7eb6f308b24adbfafd19ae86b
estebancamargo <158587958+estebancamargo@users.noreply.github.com>
2026-05-26 11:27:27 -0500

    Implement Car class with properties and Main method
    Added a Car class with properties and a Main method to create and display car objects.

---
+++ ejercicio29.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class ejercicio29
+{
+ 
+        class Car {
+            string color, model;
+            int year;
+            
+            
+            static void Main(string[] args) {
+                Car Ford = new Car();
+                Ford.color = "red"; 
+                Ford.model = "Mustang";
+                Ford.year = "1969";
+                Car Opel = new Car();
+                Opel.color = "red"; 
+                Opel.model = "Mustang";
+                Opel.year = "2005";
+                Console.WriteLine(myObj.carro);
+                Console.WriteLine("color es: " + Ford.color);
+                Console.WriteLine("modelo: " + Ford.model);
+                Console.WriteLine("año: " + Ford.year);
+
+            
+        }
+    }
+}
