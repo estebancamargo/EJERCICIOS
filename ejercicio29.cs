848d8c8f8a20566c90f8f35ac483ae4cdcf6df62
estebancamargo <158587958+estebancamargo@users.noreply.github.com>
2026-05-26 23:36:06 -0500

    Refactor Car class and update main method

--- ejercicio29.cs
+++ ejercicio29.cs
@@ -1,28 +1,46 @@
 using System;
 
-public class ejercicio29
+namespace ejercicio29
 {
- 
-        class Car {
-            string color, model;
-            int year;
-            
-            
-            static void Main(string[] args) {
-                Car Ford = new Car();
-                Ford.color = "red"; 
-                Ford.model = "Mustang";
-                Ford.year = "1969";
-                Car Opel = new Car();
-                Opel.color = "red"; 
-                Opel.model = "Mustang";
-                Opel.year = "2005";
-                Console.WriteLine(myObj.carro);
-                Console.WriteLine("color es: " + Ford.color);
-                Console.WriteLine("modelo: " + Ford.model);
-                Console.WriteLine("año: " + Ford.year);
-
-            
-        }
+  class Car
+  {
+    string color;                
+    int maxSpeed;
+    string marca;                 
+    public void fullThrottle()   
+    {
+      Console.WriteLine("The car is going as fast as it can!");
     }
+
+    public void mostrar()
+{
+  Console.WriteLine("Color: " + color);
+  Console.WriteLine("Velocidad maxima: " + maxSpeed);
+  Console.WriteLine("Marca: " + marca);
+  Console.WriteLine();
+}
+
+    static void Main(string[] args)
+    {
+      Car carro1 = new Car();
+      carro1.color = "rojo";
+      carro1.maxSpeed = 200;
+      carro1.marca = "tesla";  
+
+      Car carro2 = new Car();
+      carro2.color = "verde";
+      carro2.maxSpeed = 100;
+      carro2.marca = "BMW";
+
+      Car carro3 = new Car();
+      carro3.color = "amarillo";
+      carro3.maxSpeed = 300;
+      carro3.marca = "Audi";
+
+      carro1.mostrar();
+      carro2.mostrar();
+      carro3.mostrar();
+      
+    }
+  }
 }
